fe68be7f38921bc8c69b51178fea2ac16b48519a
flapoll1 <41431280+flapoll1@users.noreply.github.com>
2019-10-11 00:19:48 +0200

    Daten angepasst, Darstellung noch viel hübschererererer

--- client/src/app/workstation/workstation.mock.ts
+++ client/src/app/workstation/workstation.mock.ts
@@ -9,7 +9,7 @@ export const WORKSTATIONSMOCK: Workstation[] = [
   {id: 5, label: '3.14/2', telephone: true, mouse: true, headset: true, noise: 0, freeDays: ['2020-10-11', '2020-10-12']},
   {id: 6, label: '3.14/3', telephone: false, mouse: false, headset: false, noise: 1, freeDays: ['2019-10-11', '2019-10-13']},
   {id: 7, label: '3.14/4', telephone: true, mouse: false, headset: true, noise: 1, freeDays: ['2019-10-14', '2019-10-15']},
-  {id: 8, label: '3.13/1', telephone: false, mouse: false, headset: true, noise: 1, freeDays: ['2019-10-11', '2019-10-17']},
+  {id: 8, label: '3.13/1', telephone: false, mouse: false, headset: true, noise: 1, freeDays: ['2019-10-11']},
   {id: 9, label: '3.13/2', telephone: true, mouse: true, headset: false, noise: 2, freeDays: ['2019-10-01', '2019-10-15']},
   {id: 10, label: '3.13/3', telephone: true, mouse: false, headset: false, noise: 2, freeDays: ['2019-10-12', '2019-10-30']},
   {id: 11, label: '3.13/4', telephone: false, mouse: false, headset: true, noise: 2, freeDays: ['2019-10-13', '2019-10-31']},
@@ -18,5 +18,5 @@ export const WORKSTATIONSMOCK: Workstation[] = [
   {id: 14, label: '3.12/3', telephone: true, mouse: false, headset: true, noise: 2, freeDays: ['2019-10-10', '2019-10-11']},
   {id: 15, label: '3.11/1', telephone: true, mouse: false, headset: true, noise: 1, freeDays: ['2019-10-10', '2019-10-11']},
   {id: 16, label: '3.11/2', telephone: false, mouse: true, headset: true, noise: 0, freeDays: ['2019-10-10', '2019-10-11']},
-  {id: 17, label: '3.11/3', telephone: true, mouse: false, headset: true, noise: 2, freeDays: ['2019-10-10', '2019-10-11']}
+  {id: 17, label: '3.11/3', telephone: true, mouse: false, headset: true, noise: 2, freeDays: ['2019-10-10', '2019-10-11', '2019-10-12']}
 ];
