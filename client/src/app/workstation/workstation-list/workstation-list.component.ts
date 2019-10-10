85a43565bc6c9fb3b5046666b20d79b10333677e
flapoll1 <41431280+flapoll1@users.noreply.github.com>
2019-10-10 19:13:30 +0200

    Änderungen in Form werden auch ohne Submit-Button abgegeben (change)

--- client/src/app/workstation/workstation-list/workstation-list.component.ts
+++ client/src/app/workstation/workstation-list/workstation-list.component.ts
@@ -1,23 +1,25 @@
-import { Component, OnInit, Input, OnChanges } from '@angular/core';
-import { Workstation } from '../workstation.entity';
-import { FormGroup, FormControl } from '@angular/forms';
-import { startOfDay, parseISO, compareDesc } from 'date-fns/fp';
-import { WorkstationService } from '../workstation.service';
-import { Observable } from 'rxjs';
+import { Component, OnInit, Input, OnChanges } from "@angular/core";
+import { Workstation } from "../workstation.entity";
+import { FormGroup, FormControl } from "@angular/forms";
+import { startOfDay, parseISO, compareDesc } from "date-fns/fp";
+import { WorkstationService } from "../workstation.service";
+import { Observable } from "rxjs";
+import { ValueConverter } from "@angular/compiler/src/render3/view/template";
 
 @Component({
-  selector: 'app-workstation-list',
-  templateUrl: './workstation-list.component.html',
-  styleUrls: ['./workstation-list.component.css']
+  selector: "app-workstation-list",
+  templateUrl: "./workstation-list.component.html",
+  styleUrls: ["./workstation-list.component.css"]
 })
 export class WorkstationListComponent implements OnInit {
   public form: FormGroup;
   public formDate: Date;
+  public filteredWorkstations: Workstation[];
   @Input() workstationList: Workstation[];
 
   constructor(private workstationService: WorkstationService) {
     this.formDate = startOfDay(new Date());
-   }
+  }
 
   ngOnInit() {
     this.initForm();
@@ -25,27 +27,46 @@ export class WorkstationListComponent implements OnInit {
 
   initForm() {
     this.form = new FormGroup({
-      selectedDate: new FormControl(this.formDate)
+      selectedDate: new FormControl(this.formDate),
+      mouseNeeded: new FormControl(false)
     });
+    this.workstationService
+      .getAll()
+      .subscribe((result: Workstation[]) => {this.workstationList = result; });
   }
 
+  /*To DO:
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+
+  }*/
+
   onSubmit() {
-    this.workstationService.getAll().subscribe(result => {
-      this.workstationList = this.func(result);
-    });
-   // this.workstationList = this.func(result);
+    this.filteredWorkstations = this.filterByDate();
+    if (this.form.value.mouseNeeded) {
+      this.workstationList = this.filterByMouse(this.workstationList);
+    }
+
+    // this.workstationList = this.func(result);
     console.log(this.workstationList);
   }
 
-
-  /*onSelectDay($event) {
-    console.log($event.values);
-   /* this.workstationService.getAll().subscribe(result => {
+  onSelectDay(event) {
+    console.log(event.target.value);
+    this.filteredWorkstations = this.filterByDate();
+    /* this.workstationService.getAll().subscribe(result => {
       this.workstationList = this.func(result);
     });
    // this.workstationList = this.func(result);
     console.log(this.workstationList);*/
-  //}
+  }
+
+  onSelectMouse(event) {
+    if (event.target.checked) {
+    this.filteredWorkstations = this.filterByMouse(this.filteredWorkstations);
+    }
+  }
 
   containsDate(selectedDate, value: Workstation) {
     for (let freeDay of value.freeDays) {
@@ -56,18 +77,25 @@ export class WorkstationListComponent implements OnInit {
     return false;
   }
 
-    func(data: Workstation[]) {
-      var filtered = data.filter(this.containsDate.bind(this, this.form.value.selectedDate));
-      console.log(this.form.value);
-      console.log(filtered);
-      return filtered;
-      //document.write(filtered);
+  filterByDate(): Workstation[] {
+    let data = this.workstationList;
+    var filtered = data.filter(
+      this.containsDate.bind(this, this.form.value.selectedDate)
+    );
+    return filtered;
+    //document.write(filtered);
   }
 
+  hasMouse(value: Workstation) {
+    return value.mouse;
+  }
+
+  filterByMouse(data: Workstation[]) {
+    return data.filter(this.hasMouse);
+  }
 
   /*ngOnChanges() {
     console.log('OnChanges ausgelöst');
     Object.assign(this.formDate, this.form.value);
   }*/
-
 }
