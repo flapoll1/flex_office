961da62565ef84be3fb73fcf2b9c75ea111df67f
flapoll1 <41431280+flapoll1@users.noreply.github.com>
2019-10-10 18:30:16 +0200

    Mehr Mockdaten, Mapping boolean und number auf Text

--- client/src/app/workstation/workstation.mock.ts
+++ client/src/app/workstation/workstation.mock.ts
@@ -1,7 +1,22 @@
 import { Workstation } from './workstation.entity';
 
 export const WORKSTATIONSMOCK: Workstation[] = [
-  {id: 0, label: '3.15/1', telephone: true, mouse: true, headset: true, noise: 1, freeDays: ['2019-10-10', '2019-10-11']},
-  {id: 1, label: '3.15/2', telephone: false, mouse: true, headset: false, noise: 2, freeDays: ['2019-10-11', '2019-10-11']},
-  {id: 2, label: '3.15/3', telephone: true, mouse: false, headset: true, noise: 3, freeDays: ['2019-10-11', '2019-10-10']}
+  {id: 0, label: '3.15/1', telephone: true, mouse: true, headset: true, noise: 0, freeDays: ['2019-10-10', '2019-10-11']},
+  {id: 1, label: '3.15/2', telephone: false, mouse: true, headset: false, noise: 1, freeDays: ['2019-10-11', '2019-10-11']},
+  {id: 2, label: '3.15/3', telephone: true, mouse: false, headset: true, noise: 2, freeDays: ['2019-10-11', '2019-10-10']},
+  {id: 3, label: '3.15/4', telephone: true, mouse: false, headset: true, noise: 0, freeDays: ['2019-10-09', '2019-10-15']},
+  {id: 4, label: '3.14/1', telephone: true, mouse: false, headset: false, noise: 0, freeDays: ['2019-11-12', '2019-11-13']},
+  {id: 5, label: '3.14/2', telephone: true, mouse: true, headset: true, noise: 0, freeDays: ['2020-10-11', '2020-10-12']},
+  {id: 6, label: '3.14/3', telephone: false, mouse: false, headset: false, noise: 1, freeDays: ['2019-10-11', '2019-10-13']},
+  {id: 7, label: '3.14/4', telephone: true, mouse: false, headset: true, noise: 1, freeDays: ['2019-10-14', '2019-10-15']},
+  {id: 8, label: '3.13/1', telephone: false, mouse: false, headset: true, noise: 1, freeDays: ['2019-10-11', '2019-10-17']},
+  {id: 9, label: '3.13/2', telephone: true, mouse: true, headset: false, noise: 2, freeDays: ['2019-10-01', '2019-10-15']},
+  {id: 10, label: '3.13/3', telephone: true, mouse: false, headset: false, noise: 2, freeDays: ['2019-10-12', '2019-10-30']},
+  {id: 11, label: '3.13/4', telephone: false, mouse: false, headset: true, noise: 2, freeDays: ['2019-10-13', '2019-10-31']},
+  {id: 12, label: '3.12/1', telephone: true, mouse: false, headset: true, noise: 0, freeDays: ['2019-10-11', '2019-10-13']},
+  {id: 13, label: '3.12/2', telephone: true, mouse: true, headset: false, noise: 1, freeDays: ['2019-10-10', '2019-10-12']},
+  {id: 14, label: '3.12/3', telephone: true, mouse: false, headset: true, noise: 2, freeDays: ['2019-10-10', '2019-10-11']},
+  {id: 15, label: '3.11/1', telephone: true, mouse: false, headset: true, noise: 1, freeDays: ['2019-10-10', '2019-10-11']},
+  {id: 16, label: '3.11/2', telephone: false, mouse: true, headset: true, noise: 0, freeDays: ['2019-10-10', '2019-10-11']},
+  {id: 17, label: '3.11/3', telephone: true, mouse: false, headset: true, noise: 2, freeDays: ['2019-10-10', '2019-10-11']}
 ];
